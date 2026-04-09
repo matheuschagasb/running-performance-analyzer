83911829c2b33f1fbf9666ad5ec54683a20b22f2
Marcos Junior <juninhomazzero@gmail.com>
2026-04-08 23:12:31 -0300

    feat: create api to get a mp4 video

--- server/src/main.py
+++ server/src/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import shutil
+import os
 
 app = FastAPI()
+
+if not os.path.exists("videos"):
+    os.makedirs("videos")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/uploadvideo/")
+async def create_upload_file(file: UploadFile = File(...)):
+    if not file.filename.endswith('.mp4'):
+        raise HTTPException(status_code=400, detail="Invalid file type. Only .mp4 files are allowed.")
+
+    file_location = f"videos/{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+    
+    return {"info": f"File '{file.filename}' saved at '{file_location}'"}
